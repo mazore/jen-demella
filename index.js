7705f2276f0f68e226cae8852e03bfda947398fb
Evan Mazor <evanmazor@gmail.com>
2024-11-03 17:07:54 -0500

    MVP for 1.0

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzguJXhyZStKM34zHOLR4Km5RrZpODwIN6XswtesaFqWMPXMS3E9_ebbyuqkWo4ZLHOOQ/exec';
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyAzotKRZ4N4ikBtSMxHygA3kVnkI1v0CNicHix75OCkuCaICb6U4b4-KjaxWti7tnQNw/exec';
+
+const SAMPLE_ANSWERS = intakeQuestionnaire.elements.reduce((acc, element, i) => {
+    const { name } = element;
+    let text;
+    if (name == 'First Name') {
+        text = 'John';
+    } else if (name == 'Last Name') {
+        text = 'Doe';
+    } else if (name == 'Pronouns') {
+        text = 'He/Him';
+    } else {
+        text = `Template answer for ${i + 1}: ${name}`;
+    }
+    acc[name] = text;
+    return acc;
+}, {});
 
 document.addEventListener('DOMContentLoaded', function() {
     const survey = new Survey.Model(intakeQuestionnaire);
@@ -12,10 +28,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let mappedResults = {};
 
-        for (const questionKey in intakeEntryTemplateMap) {
-            if (!questionKey) continue;
-            const questionText = intakeEntryTemplateMap[questionKey];
-            mappedResults[questionKey] = result.data[questionText];
+        for (const questionTitle in intakeEntryTemplateMap) {
+            const templateKey = intakeEntryTemplateMap[questionTitle];
+            mappedResults[templateKey] = result.data[questionTitle];
+
+            if (Array.isArray(mappedResults[templateKey])) { // If multiple choice, combine array into a string with commas and 'and'
+                if (mappedResults[templateKey].length > 1) {
+                    const lastItem = mappedResults[templateKey].pop();
+                    mappedResults[templateKey] = `${mappedResults[templateKey].join(', ')} and ${lastItem}`;
+                } else {
+                    mappedResults[templateKey] = mappedResults[templateKey][0];
+                }
+            }
         }
 
         mappedResults = {
